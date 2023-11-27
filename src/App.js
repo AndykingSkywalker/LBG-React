5ad293b4530c52c37173f8fffddbce1f371eb397
andyking <andrewk640@gmail.com>
2023-11-27 15:34:17 +0000

    new commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import Trainee from './components/Trainee';
-import ExtraInfo from './components/ExtraInfo';
-import ButtonTest from './components/ButtonTest';
+import logo from "./logo.svg";
+import "./App.css";
+import Trainee from "./components/Trainee";
+import ExtraInfo from "./components/ExtraInfo";
+import ButtonTest from "./components/ButtonTest";
 
 function App() {
   return (
-      <div>
-        <Trainee name="Andrew" age="27" location="Bradford"/>
-        <ExtraInfo hobby="Gaming"/>
-        <ButtonTest/> 
-        <Trainee name="Ember" age="5" location="Cat Bed"/>
-        <ExtraInfo hobby="Meowing"/>
-        <ButtonTest/> 
-      </div>  
+    <div>
+      <Trainee name="Andrew" age="27" location="Bradford" />
+      <ExtraInfo hobby="Gaming" />
+      <ButtonTest />
+      <Trainee name="Ember" age="5" location="Cat Bed" />
+      <ExtraInfo hobby="Meowing" />
+      <ButtonTest />
+    </div>
   );
 }
 
