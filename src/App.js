afdebabac9c0c41821d2c80de4299ef935e350c2
andyking <andrewk640@gmail.com>
2023-11-28 12:36:47 +0000

    added json and imported in an array

--- src/App.js
+++ src/App.js
@@ -3,16 +3,26 @@ import "./App.css";
 import Trainee from "./components/Trainee";
 import ExtraInfo from "./components/ExtraInfo";
 import ButtonTest from "./components/ButtonTest";
+import trainees from "./data/trainees.json";
 
 function App() {
+  const traineesArray = [];
+
+  for (const trainee of trainees) {
+    console.log("Trainee:", trainee);
+    traineesArray.push(
+      <Trainee
+        name={trainee.name}
+        age={trainee.age}
+        location={trainee.location}
+      />
+    );
+  }
+
   return (
     <div>
-      <Trainee name="Andrew" age="27" location="Bradford" />
-      <ExtraInfo hobby="Gaming" />
-      <ButtonTest />
-      <Trainee name="Ember" age="5" location="Cat Bed" />
-      <ExtraInfo hobby="Meowing" />
-      <ButtonTest />
+      <h1>Meet the trainees</h1>
+      {traineesArray}
     </div>
   );
 }
